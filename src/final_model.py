76870c84c4316c62ef8f625d582da12d9cc64a44
Jakub Dadello <kejdi16try@gmail.com>
2026-01-11 19:59:30 +0100

    update final_model

--- src/final_model.py
+++ src/final_model.py
@@ -11,7 +11,6 @@ from preprocessing import preprocessor, X, Y
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 import joblib 
 
 # --- Define path to save trained model ---
@@ -25,7 +24,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random
 
 # --- flatten y to avoid DataConversionWarning ---
 y_train = y_train.values.ravel()
-y_test  = y_test.values.ravel()
 
 # --- best parameters configuration ---
 best_params = {
