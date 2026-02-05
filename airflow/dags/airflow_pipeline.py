911b59875a75d85dc0ad2da62952398aad842c6f
Jakub Dadello <kejdi16try@gmail.com>
2026-02-05 16:57:04 +0100

    update airflow_pipeline.py

--- airflow/dags/airflow_pipeline.py
+++ airflow/dags/airflow_pipeline.py
@@ -1,11 +1,11 @@
 import os
-import pandas as pd 
-from airflow import DAG 
+import pandas as pd
+from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
-from src.db_utils import load_csv_to_db, load_data
+from src.db_utils import load_csv_to_db
 from src.final_model import train_model
 from src.feature_importance import get_feature_importance
 
@@ -13,11 +13,11 @@ from src.feature_importance import get_feature_importance
 default_args = {
     "owner": "airflow",
     "retries": 1
-},
+}
 
 def task_load_csv():
     csv_path = os.getenv("CSV_PATH")
-    df = load_csv_to_db(csv_path)
+    load_csv_to_db(csv_path)
 
 def task_run_etl():
     db_url = os.getenv("PROJECT_DB")
@@ -25,45 +25,43 @@ def task_run_etl():
 
     sql_path = os.path.join(os.path.dirname(__file__), "initial_labeling.sql")
     with engine.begin() as connection:
-        connection.execute(open(sql_path).read())
+        with open(sql_path, "r") as f:
+            connection.execute(text(f.read()))
 
-def task_train_model ():
-    # --- Define path to save trained model ---
-    df = train_model("../models/pipeline_rf.joblib") 
+def task_train_model():
+    train_model("/opt/airflow/models/pipeline_rf.joblib")
 
-def task_feature_importance ():
-    df = get_feature_importance("../models/pipeline_rf.joblib")
-
+def task_feature_importance():
+    get_feature_importance("/opt/airflow/models/pipeline_rf.joblib")
 
 with DAG(
-    "airflow_pipeline.py",
-    default_args = default_args,
-    schedule_interval = None,
-    start_date = datetime(2024,1,1),
-    catchup = False
+    "airflow_pipeline",
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=datetime(2024, 1, 1),
+    catchup=False
 ) as dag:
-    
+
     load_csv = PythonOperator(
-        task_id = "load_csv",
-        python_callable = task_load_csv
+        task_id="load_csv",
+        python_callable=task_load_csv
     )
 
     run_etl = PythonOperator(
-        task_id = "run_etl",
-        python_callable = task_run_etl
+        task_id="run_etl",
+        python_callable=task_run_etl
     )
 
     train_model_ = PythonOperator(
-        task_id = "train_model",
-        python_callable = task_train_model
+        task_id="train_model",
+        python_callable=task_train_model
     )
 
     feature_importance = PythonOperator(
-        task_id = "feature_importance",
-        python_callable = task_feature_importance
+        task_id="feature_importance",
+        python_callable=task_feature_importance
     )
 
-
     load_csv >> run_etl >> train_model_ >> feature_importance
- 
+
 
